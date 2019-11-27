a476dba30e1efe92b023e69442f8dbcade7c5df7
Opetion <jfc.costa91@gmail.com>
2019-11-27 21:16:41 +0000

    :recycle: avoid constraint exception using default values

--- src/main/java/com/opetion/seagit/web/GitController.java
+++ src/main/java/com/opetion/seagit/web/GitController.java
@@ -5,14 +5,10 @@ import com.opetion.seagit.git.GitService;
 import com.opetion.seagit.git.RefCommit;
 import com.opetion.seagit.git.page.Page;
 import com.opetion.seagit.git.page.PageRequest;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/api/repositories")
-@Validated
 public class GitController {
 
 	private final GitService service;
@@ -22,9 +18,10 @@ public class GitController {
 	}
 
 	@GetMapping
-	Page<GitRepository> getRepositories(@RequestParam(defaultValue = "5") @Min(1) int size,
-			@RequestParam(defaultValue = "0") @Min(0) int page) {
-		// TODO: Contraint Violation Error is 500, make it something nicer
+	Page<GitRepository> getRepositories(@RequestParam(defaultValue = "5") int size,
+			@RequestParam(defaultValue = "0") int page) {
+		size = (size <= 0) ? 5 : size;
+		page = (page <= 0) ? 0 : page;
 		return service.find(new PageRequest(size, page));
 	}
 
@@ -34,9 +31,11 @@ public class GitController {
 	}
 
 	@GetMapping("/{id}/commits")
-	Page<RefCommit> getCommits(@PathVariable int id, @RequestParam(defaultValue = "5") @Min(1) int size,
-			@RequestParam(defaultValue = "0") @Min(0) int page) {
-		// TODO: Contraint Violation Error is 500, make it something nicer
+	Page<RefCommit> getCommits(@PathVariable int id, @RequestParam(defaultValue = "5") int size,
+			@RequestParam(defaultValue = "0") int page) {
+		size = (size <= 0) ? 5 : size;
+		page = (page <= 0) ? 0 : page;
+
 		return service.details(id, new PageRequest(size, page));
 	}
 
